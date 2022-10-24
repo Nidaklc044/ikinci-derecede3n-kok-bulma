54d30a8f85cdcfddf1cead8cca67352339e02408
Nidaklc044 <116462585+Nidaklc044@users.noreply.github.com>
2022-10-24 11:21:42 +0300

    kok bulma
    c#

---
+++ ikidenklemkokubulma/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double a, b, c, delta,kok1,kok2;
+Console.WriteLine("denklemin a degerini giriniz");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("denklemin b degerini giriniz");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("denklemin c degerini giriniz");
+c = double.Parse(Console.ReadLine());
+delta=Math.Pow(b,2)-(4*a+c);
+if (delta == 0)
+{
+    kok1 = (-b) / 2 * a;
+    Console.WriteLine("denklemin ckışık kökü vardır ve kok ="+kok1 + "dir");
+}
+else if(delta >0)
+{
+    kok1 = ((-b) + Math.Sqrt(delta) / (2 * a));
+    kok2 = ((-b) - Math.Sqrt(delta) / (2 * a));
+    Console.WriteLine("denkelmin iki kökü vardır kok1 ="+ kok1 +" kok2= "+ kok2);
+}
+else
+{
+    Console.WriteLine("kök yoktur");
+}
